694084ca0a394a56c5f47e7622360782d72258b2
roxydao97 <roxydao97@gmail.com>
2018-04-21 14:06:08 +0300

    My Django Girls app, first commit

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\roxyd\documents\oamk\files\django girls\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
